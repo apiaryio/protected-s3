2fd8d9d1781d486d3857be24a1decab46dedbaee
decaffeinate <ondrej.musil@oracle.com>
2017-07-18 15:28:45 +0200

    decaffeinate: Run post-processing cleanups on app.coffee and 6 other files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const express      = require('express');
 const session      = require('express-session');
 const path         = require('path');
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const gulp = require('gulp');
 const coffeelint = require('gulp-coffeelint');
 const mocha = require('gulp-mocha');
