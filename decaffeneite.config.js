721ce8aa5ffafb3a9a9200709025541d695e0c6c
Ondřej Musil <ondrej.musil@oracle.com>
2017-07-18 15:40:15 +0200

    chore: Removed decaffeneite config

--- decaffeneite.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  searchDirectory: './',
-  decaffeinateArgs: [
-    '--disable-suggestion-comment',
-    '--disallow-invalid-constructors',
-    '--loose-for-of',
-    '--loose-default-params',
-  ],
-};
