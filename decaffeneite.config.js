a9d9b690df7c006cf65f55033b9ddd8daf3626e0
Ondřej Musil <ondrej.musil@oracle.com>
2017-07-18 15:25:40 +0200

    feat: Added decaffeneite config

---
+++ decaffeneite.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  searchDirectory: './',
+  decaffeinateArgs: [
+    '--disable-suggestion-comment',
+    '--disallow-invalid-constructors',
+    '--loose-for-of',
+    '--loose-default-params',
+  ],
+};
